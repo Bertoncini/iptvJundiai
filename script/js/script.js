4a41f5eed18f793362b3beb027adf52eb96f3f69
Alex Bertoncini <alex.bertoncini@tbfg.com.br>
2018-08-31 09:51:56 -0300

    primeira versão IPTV JUNDIAI

---
+++ script/js/script.js
@@ -0,0 +1,9 @@
+// chat
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'd34137a2d47885acf2259df8f19c1860fc61a7fe';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
